5f8fe51b99dbe28e6f5dddd4075c0e5cdc652342
JRnonr <1437167377@qq.com>
2024-10-28 10:47:51 +0000

    add negative test

--- test_times.py
+++ test_times.py
@@ -1,4 +1,7 @@
 from times import compute_overlap_time, time_range
+import pytest
+
+
 
 def test_generic_case():
     large = time_range("2010-01-12 10:00:00", "2010-01-12 12:00:00")
@@ -34,9 +37,24 @@ def test_exact_end_start_case():
     expected = []  
     assert compute_overlap_time(large, short) == expected
 
+# def test_negative_case():
+#     large = time_range("2010-01-12 10:00:00", "2010-01-12 09:00:00")  
+#     short = time_range("2010-01-12 10:00:00", "2010-01-12 11:00:00")  
+#     expected = []  
+#     assert compute_overlap_time(large, short) == expected
+
+
+def test_time_range_backwards():
+    # 使用 pytest.raises 来检查是否抛出 ValueError
+    with pytest.raises(ValueError, match="End time must be after start time."):
+        time_range("2010-01-12 12:00:00", "2010-01-12 10:00:00")
+
 if __name__ == "__main__":
     test_generic_case()
     test_no_overlap_case()
     test_multiple_intervals_overlap_case()
     test_exact_end_start_case()
+    #test_negative_case()
+    test_time_range_backwards()
+    
     print("All tests passed!")
